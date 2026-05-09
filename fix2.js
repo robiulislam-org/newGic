d5c22b897fcc3af57049d610168a09863840ce50
robiulislam-org <robiulislam4020@gmail.com>
2026-05-09 09:27:15 +0600

    Updated files: consolidated into index.html

--- fix2.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const files = ['courses.html', 'teachers.html', 'about.html', 'contact.html', 'join-as-teacher.html', 'policy.html'];
-
-files.forEach(file => {
-    let content = fs.readFileSync(file, 'utf-8');
-    content = content.replace(/<div class="nav-toggle"[^>]*>([\s\S]*?)<\/div>/g, '<button class="nav-toggle" aria-label="Toggle Navigation">$1</button>');
-    fs.writeFileSync(file, content, 'utf-8');
-});
