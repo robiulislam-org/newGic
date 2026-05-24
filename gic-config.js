ba783734b35720418357720adf6ccbf17d85f040
robiulislam-org <robiulislam4020@gmail.com>
2026-05-24 08:54:26 +0600

    feat: Supabase analytics fully connected - live tracking enabled

--- gic-config.js
+++ gic-config.js
@@ -2,8 +2,8 @@
 // Replace the values below with your Supabase Project credentials.
 // You can get these by creating a free project at https://supabase.com/
 
-export const supabaseUrl = "PLACEHOLDER_SUPABASE_URL";
-export const supabaseAnonKey = "PLACEHOLDER_SUPABASE_ANON_KEY";
+export const supabaseUrl = "https://abpweawndpnaftkcsdcp.supabase.co";
+export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFicHdlYXduZHBuYWZ0a2NzZGNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1Njc1ODMsImV4cCI6MjA5NTE0MzU4M30.B3rV8pp0HL9xYBhGDcJGJD3b1unjtNk1ChB_4_OgW9Y";
 
 // Enter the password you want to use to access your dashboard.html
 // Make sure this matches the password in your setup.sql script
